9317220dc772aeb2eacefee96507ce996848c3c1
brilliant-ember <fire.senfony@gmail.com>
2017-12-02 17:28:20 -0400

    updated hobbies activity

--- app/src/main/java/com/example/mohammedal/learnarabic/HobbiesActivity.java
+++ app/src/main/java/com/example/mohammedal/learnarabic/HobbiesActivity.java
@@ -2,6 +2,9 @@ package com.example.mohammedal.learnarabic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HobbiesActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class HobbiesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hobbies);
+
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("Swimming","السباحة"));
+        words.add(new Word("Running","الجري"));
+        words.add(new Word("Football","كرةالقدم"));
+        words.add(new Word("Golf","الجولف"));
+        words.add(new Word("Chess","الشطرنج"));
+        words.add(new Word("Playing the Guitar","العزف على الجيتار"));
+        ListView root = findViewById(R.id.root);
+        root.setAdapter(new WordsAdapter(this, words));
     }
 }
