e1e92838d5966e36de625870e932b823d2a7ce4e
brilliant-ember <fire.senfony@gmail.com>
2017-12-02 16:57:45 -0400

    fixed naming issue with numbers images, and updated the numbers class to take pictures

--- app/src/main/java/com/example/mohammedal/learnarabic/NumbersActivity.java
+++ app/src/main/java/com/example/mohammedal/learnarabic/NumbersActivity.java
@@ -15,16 +15,16 @@ public class NumbersActivity extends AppCompatActivity {
         ArrayList<String> Words= new ArrayList<String>();
 
         ArrayList<Word> numbers= new ArrayList<Word>();
-        numbers.add(new Word("One","واحد"));
-        numbers.add(new Word("Two","اثنين"));
-        numbers.add(new Word("Three","ثلاثة"));
-        numbers.add(new Word("Four","اربعة"));
-        numbers.add(new Word("Five","خمسة"));
-        numbers.add(new Word("Six","ستة"));
-        numbers.add(new Word("Seven","سبعة"));
-        numbers.add(new Word("Eight","ثمانية"));
-        numbers.add(new Word("Nine","تسعة"));
-        numbers.add(new Word("Ten","عشرة"));
+        numbers.add(new Word("One","واحد",R.mipmap.one1));
+        numbers.add(new Word("Two","اثنين",R.mipmap.two));
+        numbers.add(new Word("Three","ثلاثة",R.mipmap.three));
+        numbers.add(new Word("Four","اربعة", R.mipmap.four));
+        numbers.add(new Word("Five","خمسة",R.mipmap.five));
+        numbers.add(new Word("Six","ستة", R.mipmap.six));
+        numbers.add(new Word("Seven","سبعة",R.mipmap.seven ));
+        numbers.add(new Word("Eight","ثمانية",R.mipmap.eight));
+        numbers.add(new Word("Nine","تسعة",R.mipmap.nine));
+        numbers.add(new Word("Ten","عشرة", R.mipmap.ten));
 
         ListView numPage = (ListView) findViewById(R.id.numPage);
         numPage.setAdapter(new WordsAdapter(this,numbers));
