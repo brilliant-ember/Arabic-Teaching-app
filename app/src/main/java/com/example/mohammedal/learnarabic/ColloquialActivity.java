54707814b16e5b79ca2b58699b4dcb11e71961be
brilliant-ember <fire.senfony@gmail.com>
2017-12-22 09:38:47 -0400

    uplaoded flag images and laucher icons

--- app/src/main/java/com/example/mohammedal/learnarabic/ColloquialActivity.java
+++ app/src/main/java/com/example/mohammedal/learnarabic/ColloquialActivity.java
@@ -19,25 +19,29 @@ public class ColloquialActivity extends AppCompatActivity {
         setContentView(R.layout.activity_colloquial);
 
         final ArrayList<Word> phrases = new ArrayList<Word>();
-        phrases.add(new Word("Yemeni for: I want this","اشتي هذا", R.raw.yashtihada));
-        phrases.add(new Word("Yemeni for: Hurry up!","فيسع!", R.raw.yfisaa));
-        phrases.add(new Word("Yemeni for: To strike or to hit","يندف", R.raw.yeindouf));
+        phrases.add(new Word("Yemeni for: I want this","اشتي هذا",R.mipmap.yem, R.raw.yashtihada));
+        phrases.add(new Word("Yemeni for: Hurry up!","فيسع!",R.mipmap.yem, R.raw.yfisaa));
+        phrases.add(new Word("Yemeni for: To strike or to hit","يندف",R.mipmap.yem, R.raw.yeindouf));
+
         phrases.add(new Word("Palestinian for: Spider web","شعشبون"));
         phrases.add(new Word("Palestinian for: Scooch over","ليز شوي"));
         phrases.add(new Word("Palestinian for: Clothes","اواعي"));
-        phrases.add(new Word("Egyption for: How are you man","ازيك يا باشا", R.raw.egyezyak));
-        phrases.add(new Word("Egyption for: How old are you?","عندك كم سنة؟ ", R.raw.egyhowoldareyou));
-        phrases.add(new Word("Egyption for: ٌٌRoom","أوضة ",R.raw.egyroom));
-        phrases.add(new Word("Egyption for: Barely","بالـزور ",R.raw.egybilzoor));
+
+        phrases.add(new Word("Egyption for: How are you man","ازيك يا باشا",R.mipmap.msr, R.raw.egyezyak));
+        phrases.add(new Word("Egyption for: How old are you?","عندك كم سنة؟ ",R.mipmap.msr ,R.raw.egyhowoldareyou));
+        phrases.add(new Word("Egyption for: ٌٌRoom","أوضة ",R.mipmap.msr,R.raw.egyroom));
+        phrases.add(new Word("Egyption for: Barely","بالـزور ",R.mipmap.msr, R.raw.egybilzoor));
+
         phrases.add(new Word("Sham region for: What do you want?","شو بدك؟ "));
         phrases.add(new Word("Sham region for: What?","شو؟ "));
         phrases.add(new Word("Sham region for: Money","مصاري"));
         phrases.add(new Word("Sham region for: I want this","بدي هاد "));
-        phrases.add(new Word("Lebanese for: What is this thing","شو هيدا الشي", R.raw.lebshuhyda));
-        phrases.add(new Word("Lebanese for: May your house get destroyed how much I loved you","يخرب بيتك شو حبيتك", R.raw.lebyhrebbytak));
-        phrases.add(new Word("Lebanese for: You came at the perfect time","جيت والله جابك", R.raw.lebjeetwaallahjabak));
-        phrases.add(new Word("Lebanese for: What's up babe","شو حياتي", R.raw.extra));
-        phrases.add(new Word("Lebanese for: You are my babe","حبيبي انتا", R.raw.lebyouaremybabe));
+
+        phrases.add(new Word("Lebanese for: What is this thing","شو هيدا الشي", R.mipmap.leb, R.raw.lebshuhyda));
+        phrases.add(new Word("Lebanese for: May your house get destroyed how much I loved you","يخرب بيتك شو حبيتك",R.mipmap.leb, R.raw.lebyhrebbytak));
+        phrases.add(new Word("Lebanese for: You came at the perfect time","جيت والله جابك", R.mipmap.leb, R.raw.lebjeetwaallahjabak));
+        phrases.add(new Word("Lebanese for: What's up babe","شو حياتي", R.mipmap.leb,R.raw.extra));
+        phrases.add(new Word("Lebanese for: You are my babe","حبيبي انتا", R.mipmap.leb,R.raw.lebyouaremybabe));
 
 
 
--- app/src/main/java/com/example/mohammedal/learnarabic/PhrasesActivity.java
+++ app/src/main/java/com/example/mohammedal/learnarabic/PhrasesActivity.java
@@ -18,9 +18,9 @@ public class PhrasesActivity extends AppCompatActivity {
         phrases.add(new Word("Come on","يلا!"));
         phrases.add(new Word("Formal hello","السلام عليكم ورحمة الله و بركاتةً"));
         phrases.add(new Word("How are you?","كيفك؟"));
-        phrases.add(new Word("My name is Mohammed","اسمي محمدً"));
-        phrases.add(new Word("I am sorry","انا اسف ً"));
-        phrases.add(new Word(".","--ً"));
+        phrases.add(new Word("My name is Mohammed","اسمي محمد"));
+        phrases.add(new Word("I am sorry","انا اسف"));
+        phrases.add(new Word("---        .       ---","ً"));
 
         phrases.add(new Word("How much does this cost?","بكم هاذي؟ً"));
         phrases.add(new Word("Thank you very much","شكراَ جزيلاًَ"));
@@ -37,7 +37,7 @@ public class PhrasesActivity extends AppCompatActivity {
         phrases.add(new Word("How are you feeling, asking a woman","كيف تشعرين؟ً"));
         phrases.add(new Word("Good luck","حظاً طيباًَ"));
         phrases.add(new Word("How can I get to the hotel?","كيف اصل الى الفندق؟ً"));
-        phrases.add(new Word("_","_ً"));
+        phrases.add(new Word("---        .       ---","ً"));
 
 
         phrases.add(new Word("Go to hell","اذهب الى الجحيمً"));
