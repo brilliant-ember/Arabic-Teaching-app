a536b52925e09aea8117f999cc31604920ad25d3
brilliant-ember <fire.senfony@gmail.com>
2017-12-22 12:50:52 -0400

    fixed a bug that accured when i removed hobbies actiity

--- app/src/main/java/com/example/mohammedal/learnarabic/MainActivity.java
+++ app/src/main/java/com/example/mohammedal/learnarabic/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         TextView phrases = findViewById(R.id.phrases);
         phrases.setOnClickListener(new phrasesListner());
 
-        TextView hobbies = findViewById(R.id.hobbies);
-        hobbies.setOnClickListener(new HobbiesListner());
+//        TextView hobbies = findViewById(R.id.hobbies);
+//        hobbies.setOnClickListener(new HobbiesListner());
 
         TextView colloquial = findViewById(R.id.colloquial);
         colloquial.setOnClickListener(new colloquialListner());
@@ -39,24 +39,24 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public class HobbiesListner implements View.OnClickListener{
-        public void onClick(View view){
-            Intent i = new Intent(MainActivity.this, HobbiesActivity.class);
-//            final ValueAnimator anime = new ValueAnimator();
-//            anime.setDuration(500);
-//            anime.setEvaluator(new ArgbEvaluator());
-//            anime.setIntValues(Color.rgb(76,207,111),Color.MAGENTA);
-//            anime.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
-//                @Override
-//                public void onAnimationUpdate(ValueAnimator animation){
-//                    TextView hobbies = findViewById(R.id.hobbies);
-//                    hobbies.setBackgroundColor( (int) anime.getAnimatedValue());
-//                }
-//            });
-//            anime.start();
-            startActivity(i);
-        }
-    }
+//    public class HobbiesListner implements View.OnClickListener{
+//        public void onClick(View view){
+//            Intent i = new Intent(MainActivity.this, HobbiesActivity.class);
+////            final ValueAnimator anime = new ValueAnimator();
+////            anime.setDuration(500);
+////            anime.setEvaluator(new ArgbEvaluator());
+////            anime.setIntValues(Color.rgb(76,207,111),Color.MAGENTA);
+////            anime.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+////                @Override
+////                public void onAnimationUpdate(ValueAnimator animation){
+////                    TextView hobbies = findViewById(R.id.hobbies);
+////                    hobbies.setBackgroundColor( (int) anime.getAnimatedValue());
+////                }
+////            });
+////            anime.start();
+//            startActivity(i);
+//        }
+//    }
 
     public class FamilyListner implements View.OnClickListener{
         public void onClick(View view){
